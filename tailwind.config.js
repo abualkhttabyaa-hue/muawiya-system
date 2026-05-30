76268d51e2053e46c6c5437ea61d6fa4032f6b0e
abualkhttabyaa-hue <abualkhttab.yaa@gmail.com>
2026-05-30 08:17:55 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          light: "#FDE68A",
+          DEFAULT: "#D4AF37",
+          dark: "#B8860B",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
