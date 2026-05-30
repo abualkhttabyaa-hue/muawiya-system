18411a815d41d513e2ad7525e3320267a9043153
abualkhttabyaa-hue <abualkhttab.yaa@gmail.com>
2026-05-30 05:14:24 +0300

    Delete app/dashboard/page.tsx

--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-// ابحث عن العنوان القديم في الكود واستبدله بـ:
-<h1 className="text-2xl md:text-3xl font-black text-white">اللوحة الرئيسية</h1>
-<p className="text-gold text-[10px] font-bold">مركز معاوية لتحفيظ القرآن الكريم وعلومه</p>
