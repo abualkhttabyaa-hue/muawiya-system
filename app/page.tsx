f807f2cc668a615e1829a30afc5aef817c794f32
abualkhttabyaa-hue <abualkhttab.yaa@gmail.com>
2026-05-30 08:00:38 +0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  // هذه الوظيفة تقوم بتحويل أي شخص يفتح الموقع مباشرة إلى صفحة تسجيل الدخول
-  redirect("/login");
-}
